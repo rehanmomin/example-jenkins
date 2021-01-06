daa687bd85410d49bb285be07b5f57b5ac87e453
Rehan Momin <rehanmomin9029@gmail.com>
2021-01-06 13:36:39 +0530

    brew

--- setup_k6.sh
+++ setup_k6.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 set -ex
 
-apt-get update
-apt-get install dirmngr --install-recommends
-apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
-echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
-apt-get update
-apt-get install k6
+brew -v
