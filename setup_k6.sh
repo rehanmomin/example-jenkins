8408da8f0e775e961c643fe76b7be8660295f35d
Rehan Momin <rehanmomin9029@gmail.com>
2021-01-06 13:17:36 +0530

    removed sudo
    removed sudo

--- setup_k6.sh
+++ setup_k6.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 set -ex
 
-sudo apt-get update
-sudo apt-get install dirmngr --install-recommends
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
+apt-get update
+apt-get install dirmngr --install-recommends
+apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
 echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
-sudo apt-get update
-sudo apt-get install k6
+apt-get update
+apt-get install k6
